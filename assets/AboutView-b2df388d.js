a6454d9fa292a183cd040eef7778b08c457abc90
Daniel <daniel.thavares@gmail.com>
2023-02-06 15:17:37 -0300

    Updates

---
+++ assets/AboutView-b2df388d.js
@@ -0,0 +1,5 @@
+import{_ as o,o as s,h as a,a as t,s as e}from"./index-b6595826.js";const n={},r={class:"container"},c=t("h5",null,"Bibliotecas",-1),_=t("pre",null,[e("      "),t("a",{href:"https://bootswatch.com/yeti/",target:"_blank"},"Boostrap Theme"),e(`
+      `),t("a",{href:"https://icons.getbootstrap.com/font/",target:"_blank"},"Boostrap Icons Font"),e(`
+      `),t("a",{href:"https://imask.js.org",target:"_blank"},"Imask"),e(`
+      `),t("a",{href:"https://day.js.org",target:"_blank"},"Day Js"),e(`
+    `)],-1),h=[c,_];function i(l,p){return s(),a("div",r,h)}const f=o(n,[["render",i]]);export{f as default};
