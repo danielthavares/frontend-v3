06974ee2574060e68d4501ff46a741ed7248645c
Daniel <daniel.thavares@gmail.com>
2023-02-07 15:30:11 -0300

    Updates

--- assets/AboutView-e1466b09.js
+++ assets/AboutView-e1466b09.js
@@ -1,4 +1,4 @@
-import{_ as o,o as s,h as a,a as t,s as e}from"./index-b6595826.js";const n={},r={class:"container"},c=t("h5",null,"Bibliotecas",-1),_=t("pre",null,[e("      "),t("a",{href:"https://bootswatch.com/yeti/",target:"_blank"},"Boostrap Theme"),e(`
+import{_ as o,o as s,h as a,a as t,s as e}from"./index-8312065c.js";const n={},r={class:"container"},c=t("h5",null,"Bibliotecas",-1),_=t("pre",null,[e("      "),t("a",{href:"https://bootswatch.com/yeti/",target:"_blank"},"Boostrap Theme"),e(`
       `),t("a",{href:"https://icons.getbootstrap.com/font/",target:"_blank"},"Boostrap Icons Font"),e(`
       `),t("a",{href:"https://imask.js.org",target:"_blank"},"Imask"),e(`
       `),t("a",{href:"https://day.js.org",target:"_blank"},"Day Js"),e(`
