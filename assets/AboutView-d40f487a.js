59c6aed3d671da8d94ab26d6223dffbc6e5d1120
Daniel <daniel.thavares@gmail.com>
2023-04-11 11:38:26 -0300

    nova versão de exemplo

---
+++ assets/AboutView-d40f487a.js
@@ -0,0 +1,5 @@
+import{_ as o,o as s,h as a,a as t,s as e}from"./index-1886e7ec.js";const n={},r={class:"container"},c=t("h5",null,"Bibliotecas",-1),_=t("pre",null,[e("      "),t("a",{href:"https://bootswatch.com/yeti/",target:"_blank"},"Bootstrap Theme"),e(`
+      `),t("a",{href:"https://icons.getbootstrap.com/font/",target:"_blank"},"Bootstrap Icons Font"),e(`
+      `),t("a",{href:"https://imask.js.org",target:"_blank"},"Imask"),e(`
+      `),t("a",{href:"https://day.js.org",target:"_blank"},"Day Js"),e(`
+    `)],-1),h=[c,_];function i(l,p){return s(),a("div",r,h)}const f=o(n,[["render",i]]);export{f as default};
